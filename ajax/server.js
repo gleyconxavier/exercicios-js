aa9fa7f5444de78af6d8e50f6c2db573c43886df
gleyconxavier <gleyconxavier@hotmail.com>
2019-02-11 02:48:22 -0400

    Updates

--- ajax/server.js
+++ ajax/server.js
@@ -6,5 +6,44 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true })) // se vier resultado de formulario
 app.use(bodyParser.json()) // se vier json
 
-app.get('/teste', (req, res) => res.send('Ok'))
+const multer = require('multer') // interpretar formulario que veio do arquivo de upload
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, '/upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('./upload'), (req, res) => {
+    upload(req, res, err => {
+        if(err) {
+            return  res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluído com sucesso.')
+    })
+}
+
+app.post('/formulario', (req, resp) => {
+    res.send({
+        ...req.body,
+        id: 7
+    })
+})
+
+app.get('/parOuImpar', (req, res) => {
+    // req.body
+    // req.query
+    // req.params
+    const par = parseInt(req.query.numero) % 2 == 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
 app.listen(8080, () => console.log('Executando...'))
---
+++ gulp/basico/gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp')
+const concat = require('gup-concat')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+
+gulp.task('default', () => {
+    gulp.start('copiar', 'fim')
+})
+
+gulp.task('copiar', ['antes1', 'antes2'], () => {
+    gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    // .pipe(transformacao1())
+    // .pipe(transformacao2())
+    .pipe(gulp.dest('pastaB'))
+})
+
+gulp.task('antes1', () => {
+    console.log('Antes 1!!!')
+})
+
+gulp.task('antes2', () => {
+    console.log('Antes 2!!!')
+})
+
+gulp.task('fim', () => {
+    console.log('Fim!!!')
+})
