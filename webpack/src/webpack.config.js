df5c8b996a9cf22504a5d4edc04d9b6c6852a616
gleyconxavier <gleyconxavier@hotmail.com>
2019-02-14 00:09:11 -0400

    Updates

--- webpack/src/webpack.config.js
+++ webpack/src/webpack.config.js
@@ -2,7 +2,26 @@ const webpack = require('webpack')
 
 module.exports = {
     mode: 'development',
-    entry: './src/principal.js'
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'style-loader', // adicionar dentro da dom a tag <style>
+                'css-loader', // interpretar os @import, url()...
+            ]
+        }]
+    }
 }
 
     
